026953e22c359c50db1a9794f5e346c9091f2447
Serkan Sevilgen <ssevilgen@gmail.com>
2014-01-19 23:32:34 +0200

    fix minor errors add may packages

--- devsetup.sh
+++ devsetup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-################ DEV MACHINE INSTALLER #################
-# Serkan Sevilgen ssevilgen@gmail.com
-# This script installs developer tools
-# Tested and used on Ubuntu 12.04 32bit / 64bit
-# No warranty
-
 echo "YOUR SYSTEM"
 cat /etc/*-release
-sys=$(uname -m)
+SYS=$(uname -m)
 
 echo "~~ DEVELOPER MACHINE INSTALLER ~~"
-echo "This script will install Screen, Git, Python tools, Node.js, R, Emacs, MongoDB, Heroku Toolbelt, AWS Cli"
+echo "This script will install Screen, Htop, Git, Python tools, Node.js, R, Emacs, RStudio Server, MongoDB, Heroku Toolbelt, AWS Cli"
 
 # ToDo Select packages or sections here
 #echo -n "Install all / Select packages [a/s] a > "
@@ -21,9 +15,11 @@ echo "This script will install Screen, Git, Python tools, Node.js, R, Emacs, Mon
 sudo apt-get update > /dev/null
 
 # CURL
+echo "~~~ INSTALLING CURL ~~~"
 sudo apt-get install -y curl
 
 # GIT
+echo "~~~ INSTALLING GIT ~~~"
 sudo apt-get install -y git  
 echo -n "Enter your Git Name > "
 read GIT_NAME
@@ -33,33 +29,44 @@ git config --global user.name $GIT_NAME
 git config --global user.email $GIT_EMAL
 
 # SCREEN
+echo "~~~ INSTALLING SCREEN ~~~"
 sudo apt-get install screen 
 
+# HTOP
+echo "~~~ INSTALLING HTOP ~~~"
+sudo apt-get install htop
+
 # Python RELATED STUFF
+echo "~~~ INSTALLING Python Stuff pip virtualenv virtualenvwrapper ~~~"
 sudo apt-get install python-software-properties python g++ make
 sudo apt-get install python-setuptools
 sudo easy_install pip
+sudo pip install setuptools --no-use-wheel --upgrade
 sudo pip install virtualenv
-sudo pip install virtualenvwrapper > /dev/null
+sudo pip install virtualenvwrapper
 
 # NODE.JS
-sudo add-apt-repository ppa:chris-lea/node.js > /dev/null
+echo "~~~ INSTALLING NODEJS ~~~"
+sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update > /dev/null
 sudo apt-get install nodejs
 
 # R
+echo "~~~ INSTALLING R ~~~"
 sudo apt-get install r-base
 
 # EMACS24
+echo "~~~ INSTALLING EMACS ~~~"
 # https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs  > /dev/null
+sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update > /dev/null
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 # RSTUDIO SERVER
+echo "~~~ INSTALLING RSTUDIO SERVER ~~~"
 sudo apt-get install gdebi-core
 sudo apt-get install libapparmor1
-if [ $SYS == "x86_64" ]: then
+if [ $SYS == "x86_64" ]; then
     wget http://download2.rstudio.org/rstudio-server-0.98.490-amd64.deb
 else
     wget http://download2.rstudio.org/rstudio-server-0.98.490-i386.deb
@@ -67,6 +74,7 @@ fi
 sudo gdebi rstudio-server-0.98.490-i386.deb
 
 # INSTALL MongoDB
+echo "~~~ INSTALLING MONGODB ~~~"
 echo "******** Installing MongoDB ********"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > /dev/null
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
@@ -74,10 +82,10 @@ sudo apt-get update > /dev/null
 sudo apt-get install mongodb-10gen
 
 # INSTALL HEROKU TOOLBELT
+echo "~~~ INSTALLING HEROKU TOOLBELT ~~~"
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh 
 
-
 # AWS CLI
+echo "~~~ INSTALLING AWS CLI ~~~"
 pip install awscli
-  
