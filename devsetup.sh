9bd1ce68d3a2e20c3fa15c923db775814a18218d
Serkan <ssevilgen@gmail.com>
2014-06-18 07:15:59 -0400

    mongodb installs 2.6

--- devsetup.sh
+++ devsetup.sh
@@ -80,7 +80,7 @@ echo "~~~ INSTALLING MONGODB ~~~"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > /dev/null
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update > /dev/null
-sudo apt-get install mongodb-10gen
+sudo apt-get install mongodb-org
 
 # INSTALL HEROKU TOOLBELT
 echo "~~~ INSTALLING HEROKU TOOLBELT ~~~"
